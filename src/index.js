713bfe810a463b94ebb4a52aac3d418bc99b5b25
Gaurav Gupta <gauravgupta7879@gmail.com>
2020-10-10 14:50:21 +0530

    adding containers and index file

--- src/index.js
+++ src/index.js
@@ -1,17 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import "babel-polyfill";
+import "offline-js";
+
+import "sanitize.css/sanitize.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap-theme.css";
+
+import reducer from "./reducers";
+import sagas from "./sagas";
+import Routes from "./routes";
+
+import React from "react";
+import { applyMiddleware, createStore } from "redux";
+import createSagaMiddleware from "redux-saga";
+import ReactDOM from "react-dom";
+import { applyRouterMiddleware, browserHistory } from "react-router";
+import { useScroll } from "react-router-scroll";
+import { syncHistoryWithStore } from "react-router-redux";
+import { Provider } from "react-redux";
+import injectTapEventPlugin from "react-tap-event-plugin";
+injectTapEventPlugin();
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(reducer, applyMiddleware(sagaMiddleware));
+
+// Begin our Index Saga
+sagaMiddleware.run(sagas);
+
+// sync history
+const history = syncHistoryWithStore(browserHistory, store);
+
+const routerProps = {
+  history,
+  render: applyRouterMiddleware(useScroll()),
+  store,
+  sagas,
+};
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <Routes {...routerProps} />
+  </Provider>,
+  document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
